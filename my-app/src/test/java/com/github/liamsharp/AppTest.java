84f9dbe989445e39f0a4b77f9a9a46ff63371a1b
Liam Sharp <liam.sharp@screamingfrog.co.uk>
2016-11-22 09:15:18 +0000

    Test to expose issue.

---
+++ my-app/src/test/java/com/github/liamsharp/AppTest.java
@@ -0,0 +1,61 @@
+package com.github.liamsharp;
+
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import net.htmlparser.jericho.Element;
+import net.htmlparser.jericho.Source;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        final String content = 
+                  "<html>"
+                + " <head> "
+                + "  <style type=\"text/css\">"
+                // This line causes the whole doc to be ignored
+                + "<!-- "
+                + "  body {"
+                + "  background-color: yellow;"
+                + "  }"
+                + "  </head> "
+                + " <body>"
+                + " <h1>foo</h1>"
+                + " </body>" 
+                + " -->"
+                + "</html>";
+        
+        final Source source = new Source(content);
+        source.fullSequentialParse();
+        final List<Element> h1s = source.getAllElements("h1");
+        assertTrue(!h1s.isEmpty());
+    }
+}
